05074764580c04c4dc4a7c9001b8929a894cd7d7
Pavithrabalakumaran <pavithrachandrasekar9360@gmail.com>
2023-08-14 13:57:56 +0000

    cp-2a(1)

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const element = (
   <div className="social-button-container">
     <div className="content-container">
       <h1 className="heading"> Social Buttons </h1>
-      <div className="button-container"></div>
-      <button class="button button-1"> Like </button>
+      <div className="button-container">
+        <button className="button button-1"> Like </button>
+        <button className="button button-2">Comment</button>
+        <button className="button button-3">Share</button>
+      </div>
     </div>
   </div>
 );
